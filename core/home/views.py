f56007427bd46273871fe59646b549423b740074
ITDevSuccess <raharisontsidiany@gmail.com>
2024-01-31 19:34:57 +0300

    Ajout de urls

--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def get(request):
+    context = {
+        'path': request.path
+    }
+    return render(request, 'home/get', context)
